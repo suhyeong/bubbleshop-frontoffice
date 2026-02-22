b22ea8e93f4c3f99a9e2acd8a79e95a4779d4161
suhyeong <lsg1454@gmail.com>
2026-02-22 22:19:03 +0900

    [NEW] MainBody 내 인기 상품/재입고 상품 리스트 조회 작업

--- src/MainBody.tsx
+++ src/MainBody.tsx
@@ -8,6 +8,8 @@ import api from "./common/commonApi";
 const MainBody = () => {
     const [ newProducts, setNewProducts ] = useState<CarouselItem[] | undefined>(undefined);
     const [ orderCloseProducts, setOrderCloseProducts ] = useState<CarouselItem[] | undefined>(undefined);
+    const [ popularityProducts, setPopularityProducts ] = useState<CarouselItem[] | undefined>(undefined);
+    const [ stockUpProducts, setStockUpProducts ] = useState<CarouselItem[] | undefined>(undefined);
 
     const divideItemCount = 4;
     const minItemCount = 2;
@@ -26,12 +28,15 @@ const MainBody = () => {
         }
 
         const fetchProducts = async () => {
-            const [newPrd, reserveClosePrd] =
+            const [newPrd, reserveClosePrd, popularityPrd, stockUpPrd] =
                 await Promise.all([getMainProductList(MAIN_PRODUCT_TYPE.NEW),
-                    getMainProductList(MAIN_PRODUCT_TYPE.RESERVE_CLOSE)]);
+                    getMainProductList(MAIN_PRODUCT_TYPE.RESERVE_CLOSE),
+                    getMainProductList(MAIN_PRODUCT_TYPE.POPULARITY), getMainProductList(MAIN_PRODUCT_TYPE.STOCK_UP)]);
 
             if (newPrd) setNewProducts(newPrd);
             if (reserveClosePrd) setOrderCloseProducts(reserveClosePrd);
+            if (popularityPrd) setPopularityProducts(popularityPrd);
+            if (stockUpPrd) setStockUpProducts(stockUpPrd);
         }
 
         void fetchProducts();
@@ -62,11 +67,19 @@ const MainBody = () => {
         <div className="main-body-div">
             {
                 newProducts && newProducts.length > 0 && <CarouselComponent divideItemCount={divideItemCount} minItemCount={minItemCount} items={newProducts} title={"신상품"}
-                                   carouselInnerType={CAROUSEL_COMPONENT_TYPE.PRODUCT} carouselAutoPlay={false} carouselAutoPlaySpeed={6000}/>
+                                   carouselInnerType={CAROUSEL_COMPONENT_TYPE.PRODUCT} carouselAutoPlay carouselAutoPlaySpeed={6000}/>
             }
             {
                 orderCloseProducts && orderCloseProducts.length > 0 && <CarouselComponent divideItemCount={divideItemCount} minItemCount={minItemCount} items={orderCloseProducts} title={"예약 마감 임박"}
                                                   carouselInnerType={CAROUSEL_COMPONENT_TYPE.PRODUCT} carouselAutoPlay carouselAutoPlaySpeed={6000}/>
+            }
+            {
+                popularityProducts && popularityProducts.length > 0 && <CarouselComponent divideItemCount={divideItemCount} minItemCount={minItemCount} items={popularityProducts} title={"인기 상품"}
+                                                                                          carouselInnerType={CAROUSEL_COMPONENT_TYPE.PRODUCT} carouselAutoPlay carouselAutoPlaySpeed={6000}/>
+            }
+            {
+                stockUpProducts && stockUpProducts.length > 0 && <CarouselComponent divideItemCount={divideItemCount} minItemCount={minItemCount} items={stockUpProducts} title={"재입고 상품"}
+                                                                                          carouselInnerType={CAROUSEL_COMPONENT_TYPE.PRODUCT} carouselAutoPlay carouselAutoPlaySpeed={6000}/>
             }
         </div>
     );
